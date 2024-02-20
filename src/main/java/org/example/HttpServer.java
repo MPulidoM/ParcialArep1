99123d6cb400e455176ba3640ab7b8b09ad8526a
MPulidoM <118181543+MPulidoM@users.noreply.github.com>
2024-02-20 16:31:31 -0500

    Add files via upload

---
+++ src/main/java/org/example/HttpServer.java
@@ -0,0 +1,88 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        while (true) {
+            try {
+                serverSocket = new ServerSocket(35000);
+            } catch (IOException e) {
+                System.err.println("Could not listen on port: 35000.");
+                System.exit(1);
+            }
+
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+            outputLine = "HTTP/1.1 200 OK\r\n"
+                    + "Content-Type: text/html\r\n"
+                    + "\r\n" +
+                    "<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "    <head>\n" +
+                    "        <title>Fachada</title>\n" +
+                    "        <meta charset=\"UTF-8\">\n" +
+                    "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                    "    </head>\n" +
+                    "    <body>\n" +
+                    "        <h1>Calculadora</h1>\n" +
+                    "        <form action=\"/hello\">\n" +
+                    "            <label for=\"name\"> Parametros :</label><br>\n" +
+                    "            <input type=\"text\" id=\"name\" name=\"name\" value=\" \"><br><br>\n" +
+                    "            <input type=\"button\" value=\"Calcular\" onclick=\"loadGetMsg()\">\n" +
+                    "        </form> \n" +
+                    "        <div id=\"getrespmsg\"></div>\n" +
+                    "\n" +
+                    "        <script>\n" +
+                    "             function loadGetMsg() {\n" +
+                    "                let nameVar = document.getElementById(\"name\").value;\n" +
+                    "                const xhttp = new XMLHttpRequest();\n" +
+                    "                xhttp.onload = function() {\n" +
+                    "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
+                    "                    this.responseText;\n" +
+                    "                }\n" +
+                    "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\n" +
+                    "                xhttp.send();\n" +
+                    "            }\n" +
+                    "        </script>\n" +
+                    "\n" +
+                    "        </script>\n" +
+                    "    </body>\n" +
+                    "</html>";
+
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+            serverSocket.close();
+        }
+    }
+
+
+
+
+
+}
